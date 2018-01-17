3c012bf5e7ebbfc117cb40d39280bdf8f49d8076
Tiloran <Bulankino@gmail.com>
2018-01-17 23:48:35 +0200

    Add start Page

---
+++ LevelStore/LevelStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LevelStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToActionPermanent(controllerName: "Product", actionName: "List");
+        }
+    }
+}
