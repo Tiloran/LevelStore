49d9845202ac6163bb4dcbc45bce7f863c659bfc
Tiloran <Bulankino@gmail.com>
2018-02-06 18:39:09 +0200

    Now can download report-order excel file

--- LevelStore/LevelStore/Controllers/OrderController.cs
+++ LevelStore/LevelStore/Controllers/OrderController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DrawingCore;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +10,9 @@ using LevelStore.Models.Enums;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace LevelStore.Controllers
 {
@@ -91,15 +96,98 @@ namespace LevelStore.Controllers
             return Json("Fail");
         }
 
-        public IActionResult DownloadExcel()
+        public IActionResult DownloadExcel(int[] checkedOrderId)
         {
-            // Путь к файлу
-            string file_path = Path.Combine(_appEnvironment.ContentRootPath, "wwwroot/images/img/about/brand-logo.png");
-            // Тип файла - content-type
-            string file_type = "application/png";
-            // Имя файла - необязательно
-            string file_name = "brand-logo.png";
-            return PhysicalFile(file_path, file_type, file_name);
+            List<Order> orderList = repository.Orders.Where(oi => checkedOrderId.Any(coi => coi.Equals(oi.OrderID)))
+                .ToList();
+            string sWebRootFolder = _appEnvironment.WebRootPath + "/excelReports";
+            string sFileName = DateTime.Now.ToString(CultureInfo.InvariantCulture)
+                .Replace("  ", "_").Trim()
+                .Replace(" ", "_")
+                .Replace('/', '_')
+                .Replace(':', '_')
+                + "_Report.xlsx";
+            
+            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+            {
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                
+                IFont boldFont = workbook.CreateFont();
+                boldFont.Boldweight = (short)FontBoldWeight.Bold;
+                ICellStyle boldStyle = workbook.CreateCellStyle();
+                boldStyle.SetFont(boldFont);
+
+                ICell cell;
+                ISheet excelSheet = workbook.CreateSheet("Заказы");
+                excelSheet.DefaultColumnWidth = 20;
+                IRow row = excelSheet.CreateRow(0);
+                cell = row.CreateCell(0);
+                cell.SetCellValue("Имя");
+                cell.CellStyle = boldStyle;
+                cell = row.CreateCell(1);
+                cell.SetCellValue("Телефон");
+                cell.CellStyle = boldStyle;
+                cell = row.CreateCell(2);
+                cell.SetCellValue("Продукт");
+                cell.CellStyle = boldStyle;
+                cell = row.CreateCell(5);
+                cell.SetCellValue("Статус");
+                cell.CellStyle = boldStyle;
+
+                
+                int globalIndexRow = 0;
+                for (int i = 1; i <= orderList.Count; i++)
+                {
+
+                    row = excelSheet.CreateRow(i + globalIndexRow);
+                    row.CreateCell(0).SetCellValue(orderList[i-1].FirstName + " " + orderList[i - 1].LastName);
+                    row.CreateCell(1).SetCellValue(orderList[i-1].Phone);
+
+                    cell = row.CreateCell(2);
+                    cell.SetCellValue("Название продукта");
+                    cell.CellStyle = boldStyle;
+                    cell = row.CreateCell(3);
+                    cell.SetCellValue("Количество");
+                    cell.CellStyle = boldStyle;
+                    cell = row.CreateCell(4);
+                    cell.SetCellValue("Цена");
+                    cell.CellStyle = boldStyle;
+                    
+                    if (orderList[i - 1].Status == (int) OrderStatus.Waiting)
+                    {
+                        row.CreateCell(5).SetCellValue("Ожидание");
+                    }
+                    else if (orderList[i - 1].Status == (int) OrderStatus.NotSended)
+                    {
+                        row.CreateCell(5).SetCellValue("Не отослано");
+                    }
+                    else
+                    {
+                        row.CreateCell(5).SetCellValue("Отослано");
+                    }
+                    foreach (var line in orderList[i-1].Lines)
+                    {
+                        globalIndexRow++;
+                        row = excelSheet.CreateRow(i + globalIndexRow);
+                        row.CreateCell(2).SetCellValue(line.Product.Name);
+                        row.CreateCell(3).SetCellValue(line.Quantity);
+                        row.CreateCell(4).SetCellValue(line.Product.Price.ToString("C"));
+                    }
+                    globalIndexRow++;
+                    row = excelSheet.CreateRow(i + globalIndexRow);
+                    cell = row.CreateCell(2);
+                    cell.SetCellValue("Общая цена");
+                    cell.CellStyle = boldStyle;
+                    row.CreateCell(3).SetCellValue(orderList[i-1].Lines.Sum(s => s.Quantity * s.Product.Price).ToString("C"));
+
+                    
+                }
+                workbook.Write(fs);
+            }
+
+            string file_path = Path.Combine(_appEnvironment.ContentRootPath, $"wwwroot/excelReports/{sFileName}");
+            string file_type = "application/xlsx";
+            return PhysicalFile(file_path, file_type, sFileName);
         }
 
     }
