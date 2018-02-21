bb85b85118da3eb112231f507929ca8a76de6562
Tiloran <Bulankino@gmail.com>
2018-02-21 15:27:08 +0200

    Add second master page and some fixes

---
+++ LevelStore/LevelStore/Components/CategoriesViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LevelStore.Models;
+using LevelStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LevelStore.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly IProductRepository repository;
+
+        public CategoriesViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            IEnumerable<Category> categories = repository.GetCategoriesWithSubCategories();
+
+            return View("CategoriesAdmin",categories);
+        }
+    }
+}
