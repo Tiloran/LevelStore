4bb9d581248c25f7ab3a8cf0ef9469b5381180e2
ZhenyaGavr <evg.gavryliuk@gmail.com>
2018-03-12 23:30:05 +0200

    single item and mob menu

--- LevelStore/LevelStore/wwwroot/js/main.js
+++ LevelStore/LevelStore/wwwroot/js/main.js
@@ -1,20 +1,22 @@
 
 $('.carousel').carousel();
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider-for').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.slider-nav'
+
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
     });
     $('.slider-nav').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for',
-      dots: true,
-      centerMode: true,
-      focusOnSelect: true
+        slidesToShow: 3,
+        vertical: true,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true
     });
-    });
+});
